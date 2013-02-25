1c4195621a0801a8c5641ef720f49e4300594290
seemethere <seemethere101@gmail.com>
2013-02-24 20:37:20 -0600

    Refactoring

--- src/com/github/seemethere/DeathCharge/DeathCharge.java
+++ src/com/github/seemethere/DeathCharge/DeathCharge.java
@@ -14,8 +14,7 @@ import java.io.File;
 import java.util.logging.Logger;
 
 /**
- * User: Seemethere
- * Date: 2/24/13
+ * @author seemethere
  */
 public class DeathCharge extends JavaPlugin implements Listener {
 
