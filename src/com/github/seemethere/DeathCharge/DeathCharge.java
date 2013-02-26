4a7f171804f0ce839f018efabcd94ffac18983ea
seemethere <seemethere101@gmail.com>
2013-02-26 13:33:45 -0600

    Changelog 2.26.13:
    * Features added:
    	* Ability to exlude regions from the plugin using an in-game command
    	* Regions are world-specific and are added to a customized config on disabling the server
    
    * Fixes:
    	* Made creating the custom config safer
    	* Made errors caused by configs or custom configs more noticable
    
    * Refactored

--- src/com/github/seemethere/DeathCharge/DeathCharge.java
+++ src/com/github/seemethere/DeathCharge/DeathCharge.java
@@ -1,7 +1,13 @@
 package com.github.seemethere.DeathCharge;
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,6 +17,9 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -20,42 +29,168 @@ public class DeathCharge extends JavaPlugin implements Listener {
 
     private static final String PLUGIN_NAME = "[DeathCharge] ";
     public Logger logger = Logger.getLogger("Minecraft");
+    private HashMap<String, String> ex_regions = new HashMap<String, String>();
     private Economy e = null;
+    private WorldGuardPlugin wg = null;
+    private YamlConfiguration r_config = null;
 
     public void onEnable() {
-        LoadConfig();
+        //Make initial config and ExculdedRegions.yml
+        if(!make_Config()) {
+            logger.severe(PLUGIN_NAME + "Unable to create config or ExcludedRegion.yml! Exiting...");
+            return;
+        }
+        //Set up economy
         RegisteredServiceProvider<Economy> economyP = getServer().getServicesManager().getRegistration(Economy.class);
         if (economyP != null)
             e = economyP.getProvider();
         else
             logger.severe(PLUGIN_NAME + "Unable to initialize Economy Interface with Vault!");
+
+        //Set up WorldGuard
+        if (!(this.getServer().getPluginManager().getPlugin("WorldGuard") instanceof WorldGuardPlugin)) {
+            logger.severe(PLUGIN_NAME + " No WorldGuard found! Exiting via exception...");
+            throw new RuntimeException();
+        }
+        wg = (WorldGuardPlugin) this.getServer().getPluginManager().getPlugin("WorldGuard");
+
+        //Load custom config for region exclusions
+        File r_file = new File(this.getDataFolder(), "ExcludedRegions.yml");
+        try {
+            r_config = YamlConfiguration.loadConfiguration(r_file);
+        } catch (Throwable t) {
+            logger.severe(PLUGIN_NAME + "Unable to load ExcludedRegions.yml! Exiting...");
+            return;
+        }
+
+        //Populates HashMap for ExcludedRegions
+        if (r_config.getConfigurationSection("ex_regions") != null) {
+            Map<String, Object> temp = r_config.getConfigurationSection("ex_regions").getValues(false);
+            for (Map.Entry<String, Object> entry : temp.entrySet())
+                ex_regions.put(entry.getKey(), (String) entry.getValue());
+        }
         this.getServer().getPluginManager().registerEvents(this, this);
-        logger.info(PLUGIN_NAME + "has been enabled!");
+        logger.info(PLUGIN_NAME + "DeathCharge has been enabled!");
     }
 
     public void onDisable() {
-        logger.info(PLUGIN_NAME + "has been enabled!");
+        save_Config();
+        e = null;
+        wg = null;
+        ex_regions = null;
+        r_config = null;
+        logger.info(PLUGIN_NAME + "DeathCharge has been disabled!");
+        logger = null;
     }
 
-    private void LoadConfig() {
+    private boolean make_Config() {
         File pluginFolder = getDataFolder();
         if (!pluginFolder.exists() && !pluginFolder.mkdir()) {
-            logger.info(PLUGIN_NAME + "Could not make plugin folder!");
-            return;
+            logger.severe(PLUGIN_NAME + "Could not make plugin folder!");
+            return false;
         }
         File config = new File(getDataFolder(), "config.yml");
         if (!config.exists())
             this.saveDefaultConfig();
+        File r_file = new File(this.getDataFolder(), "ExcludedRegions.yml");
+        if (!r_file.exists()) {
+            try {
+                if (!r_file.createNewFile())
+                    return false;
+            } catch (IOException e) {
+                logger.severe(PLUGIN_NAME + "Could not make ExcludedRegions.yml file!");
+            }
+        }
+        return true;
+    }
+
+    private void save_Config() {
+        File r_file = new File(this.getDataFolder(), "ExcludedRegions.yml");
+        r_config.createSection("ex_regions");
+        r_config.set("ex_regions", ex_regions);
+        try {
+            r_config.save(r_file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ProtectedRegion find_Region(Location l) {
+        ProtectedRegion h = null;
+        for (ProtectedRegion r : wg.getRegionManager(l.getWorld()).getApplicableRegions(l))
+            if (h == null || h.getPriority() <= r.getPriority())
+                h = r;
+        if (h == null)
+            return null;
+        else
+            return h;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String args[]) {
+        if (!(sender instanceof Player)) {
+            logger.info(PLUGIN_NAME + "Silly console you can't do any commands");
+            return true;
+        }
+
+        Player p = (Player) sender;
+        if (command.getName().equalsIgnoreCase("deathcharge"))
+            if (args.length == 0 || args.length > 1 || !args[0].equalsIgnoreCase("region"))
+                about(p);
+            else
+                c_region(p);
+        return true;
+    }
+
+    private void c_region(Player p) {
+        if (!p.isOp()) {
+            p.sendMessage(String.format("%sERROR: %sInsufficient permissions!",
+                    ChatColor.RED, ChatColor.YELLOW));
+            return;
+        }
+        if (find_Region(p.getLocation()) != null) {
+            String id = find_Region(p.getLocation()).getId();
+            for (String s : ex_regions.keySet())
+                if (id.equalsIgnoreCase(s) && ex_regions.get(id).equalsIgnoreCase(p.getWorld().toString())) {
+                    ex_regions.remove(id.toLowerCase());
+                    p.sendMessage(String.format("%s[DeathCharge]%s Removed %s%s%s from excluded regions!",
+                            ChatColor.YELLOW, ChatColor.RED, ChatColor.YELLOW, id, ChatColor.RED));
+                    logger.info(PLUGIN_NAME + "Player " + p.getName() + " removed region " + id + " from excluded regions");
+                    return;
+                }
+            ex_regions.put(id.toLowerCase(), p.getWorld().toString());
+            p.sendMessage(String.format("%s[DeathCharge]%s Added %s%s%s to excluded regions!",
+                    ChatColor.YELLOW, ChatColor.RED, ChatColor.GREEN, id, ChatColor.RED));
+            logger.info(PLUGIN_NAME + "Player " + p.getName() + " added region " + id + " to excluded regions");
+        } else {
+            p.sendMessage(String.format("%sERROR: %sNo region found!",
+                    ChatColor.RED, ChatColor.YELLOW));
+        }
+    }
+
+    private void about(Player p) {
+        p.sendMessage(String.format("%s[DeathCharge]%s by seemethere",
+                ChatColor.YELLOW, ChatColor.RED));
+        p.sendMessage(String.format("%s[DeathCharge]%s Available on BukkitDev!",
+                ChatColor.YELLOW, ChatColor.RED));
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player p = event.getEntity();
-        double percentLost = this.getConfig().getInt("percent");
-        percentLost /= 100;
+        //Checks if the area the player is standing in is excluded
+        if (find_Region(p.getLocation()) != null) {
+            String r = find_Region(p.getLocation()).getId().toLowerCase();
+            if (ex_regions.containsKey(r) //Added to relate regions to the world they are located in
+                    && ex_regions.get(r).equalsIgnoreCase(p.getWorld().toString()))
+                return;
+        }
+        //Actually take the money from the account
+        double p_lost = this.getConfig().getDouble("percent");
+        p_lost /= 100;
         double b = e.getBalance(p.getName());
-        b *= percentLost;
+        b *= p_lost;
         e.withdrawPlayer(p.getName(), b);
-        p.sendMessage(String.format("%sYou have lost %s%.2f%s on death!", ChatColor.YELLOW, ChatColor.RED, b, ChatColor.YELLOW));
+        p.sendMessage(String.format("%sYou have lost %s%.2f%s on death!",
+                ChatColor.YELLOW, ChatColor.RED, b, ChatColor.YELLOW));
     }
 }
